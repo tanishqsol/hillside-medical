f1a33dd21e7a7bb58adab1ea1614f2ca121214d2
tanishq solanki <38584158+tanishqsol@users.noreply.github.com>
2025-02-28 16:25:55 -0500

    Switch to relative paths

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hillside-medical/',
+  base: './',
   build: {
     outDir: 'docs',
     assetsDir: 'assets',
