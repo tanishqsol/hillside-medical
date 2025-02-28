87b45f5c4de7fd1134224dc6193358e8697e33a6
tanishq solanki <38584158+tanishqsol@users.noreply.github.com>
2025-02-28 15:39:11 -0500

    Update deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hillside/', // This should match your repository name
+  base: '/hillside/', // Must match your repository name exactly
 })
