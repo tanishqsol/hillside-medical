6b83601049f99effeec9a03b0ae103b3564a7019
tanishq solanki <38584158+tanishqsol@users.noreply.github.com>
2025-02-28 15:47:17 -0500

    Update repository name in configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hillside/', // Must match your repository name exactly
+  base: '/hilside-medical/', // Must match your repository name exactly
 })
