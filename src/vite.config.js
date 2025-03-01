df13fb584c275e18ff8d85c13f11201933a652b4
tanishq solanki <38584158+tanishqsol@users.noreply.github.com>
2025-02-28 19:48:52 -0500

    jsx

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,17 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      include: "**/*.{jsx,tsx}",
-      babel: {
-        plugins: [],
-        presets: []
-      }
-    })
-  ],
+  plugins: [react()],
   base: './',
   server: {
     port: 3000
   }
-})
+}) 
